8dc4b9b93f429926c312d4253a66ef36a5c1f3ee
Ben Michaud <ben@tigersway.net>
2026-02-13 16:52:49 +0700

    First try after a long while

--- getGithubData.js
+++ getGithubData.js
@@ -1,19 +1,21 @@
 require('dotenv').config();
 
-const
-  EleventyFetch = require('@11ty/eleventy-fetch'),
+const EleventyFetch = require('@11ty/eleventy-fetch'),
   { Octokit } = require('@octokit/core'),
-  { paginateGraphql } = require("@octokit/plugin-paginate-graphql"),
+  { paginateGraphql } = require('@octokit/plugin-paginate-graphql'),
   pAll = require('p-all'),
   { green, red } = require('colorette'),
-  { writeFile } = require('fs/promises'),
+  { writeFile } = require('fs/promises');
 
-  octokit = new (Octokit.plugin(paginateGraphql))({ auth: process.env.GH_ACCESS_TOKEN });
+const octokit = new (Octokit.plugin(paginateGraphql))({
+  auth: process.env.GH_ACCESS_TOKEN
+});
 
-const wait = (s = 1, value = s) => new Promise(resolve => setTimeout(() => resolve(value), s * 1000));
+const wait = (s = 1, value = s) => new Promise((resolve) => setTimeout(() => resolve(value), s * 1000));
 
 const getRepos = async (query) => {
   try {
+    /* cSpell:disable */
     const results = await octokit.graphql.paginate(
       `query ($queryStr: String!, $cursor: String) {
         search(query: $queryStr, type: REPOSITORY, first: 100, after: $cursor) {
@@ -50,31 +52,40 @@ const getRepos = async (query) => {
       }`,
       { queryStr: query }
     );
+    /* cSpell:enable */
     console.log(`  "${query}" : ${results.search.nodes.length}`);
-    await wait(12);  // So far, impossible to be free of the "second rate limit", but ~= 2 seconds/page seems to make it work!
+    await wait(12); // So far, impossible to be free of the "second rate limit", but ~= 2 seconds/page seems to make it work!
     return results.search.nodes;
   } catch (e) {
     Promise.reject(`[${e.status}] ${e.response.data.message}`);
-  };
+  }
 };
 
 const getAllRepos = async () => {
-
   const assets = new EleventyFetch.AssetCache('github-repos');
-  if (assets.isCacheValid('6d')) return {
-    repos: await assets.getCachedValue(),
-    cachedAt: new Date(assets.cache._persisted[assets.hash].cachedAt).toISOString()
-  };
+  if (assets.isCacheValid('6d'))
+    return {
+      repos: await assets.getCachedValue(),
+      cachedAt: new Date(assets.cache._persisted[assets.hash].cachedAt).toISOString()
+    };
 
-  const now = (new Date()).toJSON();
+  const now = new Date().toJSON();
 
   // @octokit won't return more than 1000 objects, we need to split our request
-  let repos = await pAll([
-    () => getRepos('topic:eleventy stars:>0 sort:updated'),
-    () => getRepos('topic:eleventy stars:<1 sort:updated'),
-    () => getRepos('topic:11ty -topic:eleventy sort:updated')
-  ], { concurrency: 1 }) // It seems impossible to run them simultaneously on Netlify! The famous "second rate limit".
-    .then(values => values.flat());
+  let repos = await pAll(
+    [
+      // v1.2.0
+      // () => getRepos('topic:eleventy stars:>0 sort:updated'),
+      // () => getRepos('topic:eleventy stars:<1 sort:updated'),
+      // () => getRepos('topic:11ty -topic:eleventy sort:updated')
+      // v1.3.0
+      () => getRepos('eleventy OR 11ty in:topics stars:>2 sort:updated'),
+      () => getRepos('eleventy OR 11ty in:topics stars:1..2 sort:updated'),
+      () => getRepos('eleventy OR 11ty in:topics stars:0 sort:updated'),
+      () => getRepos('topic:eleventy-plugin -topic:eleventy -topic:11ty sort:updated')
+    ],
+    { concurrency: 1 } // It seems impossible to run them simultaneously on Netlify! The famous "second rate limit".
+  ).then((values) => values.flat());
 
   // Duplicates: it seems github GraphQL gives back increasing number of duplicate
   // A sort option on the query seems to have solved that problem.
@@ -82,15 +93,15 @@ const getAllRepos = async () => {
     a[e.nameWithOwner] = ++a[e.nameWithOwner] || 0;
     return a;
   }, {});
-  const duplicates = repos.filter(e => lookup[e.nameWithOwner]);
+  const duplicates = repos.filter((e) => lookup[e.nameWithOwner]);
   if (duplicates) console.log(red(`--- ${duplicates.length / 2} duplicates.`));
 
   // And now: cleaning, flattening & sorting all these repositories
-  repos = [...new Map(repos.map(repo => [repo.nameWithOwner, repo])).values()];
-  repos.forEach(repo => {
+  repos = [...new Map(repos.map((repo) => [repo.nameWithOwner, repo])).values()];
+  repos.forEach((repo) => {
     // Topics
     if (repo.topics.totalCount > 30) console.log(red(`${repo.nameWithOwner} has ${repo.topics.totalCount} topics!`));
-    repo.topics = repo.topics.nodes.map(o => o.topic.name);
+    repo.topics = repo.topics.nodes.map((o) => o.topic.name);
     // Languages
     if (repo.languages.totalCount > 12) console.log(red(`${repo.nameWithOwner} has ${repo.languages.totalCount} languages!`));
     repo.languages = repo.languages.nodes;
@@ -98,15 +109,13 @@ const getAllRepos = async () => {
     repo.search = repo.owner.login.toLowerCase() + ' ' + repo.topics.join(' ') + ' ' + repo.description?.toLowerCase();
   });
   // Back to default sorting: stargazerCount DESC + pushedAt DESC
-  repos.sort((a, b) => (b.stargazerCount - a.stargazerCount || -a.pushedAt.localeCompare(b.pushedAt)));
+  repos.sort((a, b) => b.stargazerCount - a.stargazerCount || -a.pushedAt.localeCompare(b.pushedAt));
 
   assets.save(repos, 'json');
   return { repos, cachedAt: new Date().toISOString() };
 };
 
-
 (async () => {
-
   console.log(green('--- Get Eleventy related repositories'));
   const { repos, cachedAt } = await getAllRepos();
   console.log(`--- ${repos.length} repos.`);
@@ -115,5 +124,4 @@ const getAllRepos = async () => {
   await writeFile('./site/_data/github.json', JSON.stringify({ cachedAt }, null, 2)); // Keep fetch time
 
   console.log();
-
 })();
